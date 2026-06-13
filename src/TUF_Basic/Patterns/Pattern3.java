907ba04845c63237f9784437167024e7377637d7
harshavardhanponduru <harshavardhanponduru@gmail.com>
2026-06-13 18:03:03 +0530

    Number Triangle

---
+++ src/TUF_Basic/Patterns/Pattern3.java
@@ -0,0 +1,27 @@
+package TUF_Basic.Patterns;
+
+public class Pattern3
+{
+    public static void pattern3(int n) {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        Pattern3.pattern3(5);
+    }
+}
+
+// Output: N = 4
+
+// 1
+// 12
+// 123
+// 1234
